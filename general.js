1e8aaef757929d529265af6f43eece8799383907
SeppiBrocki <150525379+SeppiBrocki@users.noreply.github.com>
2023-11-13 22:36:40 +0100

    Update general.js
    Funktionsabruf an Dateneingabe angepasst

--- general.js
+++ general.js
@@ -4,17 +4,33 @@ document.getElementById('umwandelnButton').addEventListener('click', function ()
     const gruppenAnzahl = parseInt(document.getElementById('gruppenAnzahl').value);
     const kopieText1 = document.getElementById('tabellenKopie1').value.trim();
     const kopieText2 = document.getElementById('tabellenKopie2').value.trim();
-    diff (gruppenAnzahl, kopieText1, kopieText2);
-    
-    const g = parseInt(document.getElementById('gruppenAnzahl').value);
-    console.log(g);
     const zeilen1a = kopieText1.split('\n');
     const zeilen1b = kopieText1.split('\n');
-    console.log(zeilen1a);
+    const zeilen2b = kopieText2.split('\n');
+    
+    const g = parseInt(document.getElementById('gruppenAnzahl').value);
     const kopieText3 = document.getElementById('tabellenKopie3').value.trim();
     const zeilen3 = kopieText3.split('\n');
-    console.log(zeilen3)
-    const praefListe = createSublists (zeilen3);
+    console.log(zeilen1a.length);
+    console.log(zeilen2b.length);
+    console.log(zeilen3.length);
+    if (zeilen1a.length > 1){
+        const randomFinal = random (zeilen1b,g);
+        const randomgruppenUl = document.getElementById('randomGruppen');
+    randomgruppenUl.innerHTML = '';
+    randomFinal.forEach((gruppe, index) => {
+        const gruppenLi = document.createElement('li');
+        gruppenLi.textContent = `Gruppe ${index + 1}: ${gruppe.join(', ')}`;
+        randomgruppenUl.appendChild(gruppenLi);
+                });
+    }
+
+    if (zeilen1a.length > 1 && zeilen2b.length >1) {
+        diff (gruppenAnzahl, kopieText1, kopieText2);
+    }
+
+    if (zeilen1a.length > 1 && zeilen3.length > 1) {
+        const praefListe = createSublists (zeilen3);
     console.log(praefListe);
     let susListe = mergeLists (zeilen1a, praefListe);
     console.log (susListe)
@@ -26,9 +42,6 @@ document.getElementById('umwandelnButton').addEventListener('click', function ()
     console.log(praeffinal);
     const diafinal = prepPraefDia(finaldia);
     console.log(diafinal);
-    const randomFinal = random (zeilen1b,g)
-    console.log(randomFinal);
-    
     const praefgruppenUl = document.getElementById('praefGruppen');
     praefgruppenUl.innerHTML = '';
     praeffinal.forEach((gruppe, index) => {
@@ -45,17 +58,12 @@ document.getElementById('umwandelnButton').addEventListener('click', function ()
         diagruppenUl.appendChild(gruppenLi);
                 });
 
-    const randomgruppenUl = document.getElementById('randomGruppen');
-    randomgruppenUl.innerHTML = '';
-    randomFinal.forEach((gruppe, index) => {
-        const gruppenLi = document.createElement('li');
-        gruppenLi.textContent = `Gruppe ${index + 1}: ${gruppe.join(', ')}`;
-        randomgruppenUl.appendChild(gruppenLi);
-                });
+    }
+  
+})
+
 
     
-    
-})
 
 
     
